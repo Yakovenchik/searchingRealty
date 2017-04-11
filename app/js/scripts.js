f3d345039e7d5a27684442dab8cc34b34b9f0f73
Oleg <vitiukoleh@gmail.com>
2017-04-11 03:29:35 +0300

    radio is ready but pf meaning lost

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -2,6 +2,16 @@ $(document).ready(function(){
 	$("select").selecter({
 		label: "Киев"
 	});
+	$(".choose__rooms_item").on("click",function () {
+		if($(".choose__rooms_item").hasClass("checked")){
+			$(this).css("background-color", "transparent").css("color", "#FFFFFF").removeClass("checked");
+		}
+		else{
+			$(this).css("background-color", "#FFFFFF").css("color", "#152935").addClass("checked");
+		}
+	});
+
+
 	// $(".selecter-selected").on("click",function () {
 	// 	alert("dsgdsgf");
 	// 	$(this).css("background", "url(\"./images/selectArrowOutwards.png no-repeat fixed\")");
