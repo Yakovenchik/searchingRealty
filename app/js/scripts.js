5f1b19bccc49e5fb22f4d291ee1ce4c07039de5e
Oleg <vitiukoleh@gmail.com>
2017-04-12 18:27:06 +0300

    fucking select is done

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -22,9 +22,10 @@ $(document).ready(function(){
 	})
 	$(".select").on("click",function () {
 		if($(this).hasClass("unchecked")) {
-			$(".realty_search_content_select-item").css("display", "block").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63");
+			$(".realty_search_content_select-item").css("display", "block").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63")
+				.css("box-shadow","0 5px 10px rgba(0,0,0,0.22)");
 			$(this).removeClass("unchecked");
-			$(".selectVisibleArrow").attr("src","images/selectArrowOutwards.png");
+			$(".selectVisibleArrow").attr("src","../images/selectArrowOutwards.png");
 			$(this).css("border-top","2px solid #30AE63").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63").
 				css("border-radius",0);
 			$(".realty_search_content_select-item").last().css("border-bottom","2px solid #30AE63");
@@ -42,7 +43,6 @@ $(document).ready(function(){
 		$(".select").css("border","2px solid #dfe3e6");
 	});
 	$(".realty_search_content_select-item").on("click",function () {
-
 		var colorOfBranchMetro = {};
 		colorOfBranchMetro["Политехнический институт"] = "red";
 		colorOfBranchMetro["Шулявская"] = "red";
@@ -54,6 +54,11 @@ $(document).ready(function(){
 			if(name==selectedItem){
 				$(".select .selectVisibleText").text(selectedItem);
 				$(".select .colorOfMetro").removeClass("red blue green").addClass(value);
+				$(".realty_search_content_select-item").css("display", "none");
+				$(".select").addClass("unchecked");
+				$(".selectVisibleArrow").attr("src","images/selectArrow.png");
+				$(".select").css("border-top","2px solid #dfe3e6").css("border-left","2px solid #dfe3e6").css("border-right","2px solid #dfe3e6").
+				css("border-radius","3px");
 			}
 		}
 	});
