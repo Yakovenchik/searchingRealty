345e4b813bc6a7d5f86e71b2a6ef7a24e97b7b92
Oleg <vitiukoleh@gmail.com>
2017-04-12 05:20:17 +0300

    almost done selector

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$("select").selecter({
+	$(".choose__city_selector").selecter({
 		label: "Киев"
 	});
 	$(".choose__rooms_item").on("click",function () {
@@ -9,7 +9,34 @@ $(document).ready(function(){
 		else{
 			$(this).css("background-color", "#FFFFFF").css("color", "#152935").addClass("checked");
 		}
+	})
+	$(".select").on("click",function () {
+		if($(".select").hasClass("unchecked")) {
+			$(".realty_search_content_select-item").css("display", "block");
+			$(".select").removeClass("unchecked");
+			$(".selectVisibleArrow").attr("src","images/selectArrowOutwards.png");
+			$(".select").css("border-top","2px solid #30AE63").css("border-left","2px solid #30AE63").css("border-right","2px solid #30AE63").
+				css("border-radius",0);
+		}
+		else{
+			$(".realty_search_content_select-item").css("display", "none");
+			$(".select").addClass("unchecked");
+			$(".selectVisibleArrow").attr("src","images/selectArrow.png");
+			$(".select").css("border-top","2px solid #dfe3e6").css("border-left","2px solid #dfe3e6").css("border-right","2px solid #dfe3e6").
+			css("border-radius","3px");
+		}
 	});
+	$(".select").on("hover",function () {
+		
+	});
+	$(".realty_search_content_select-item").on("click",function () {
+		
+	});
+
+
+
+
+	//$(".main").on
 
 
 	// $(".selecter-selected").on("click",function () {
